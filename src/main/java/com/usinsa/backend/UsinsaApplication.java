97c6538cabb224e75e7841aeede3bbb3e9014dfc
kozae00 <rhwogus777@gmail.com>
2025-09-22 13:22:53 +0900

    init

---
+++ src/main/java/com/usinsa/backend/UsinsaApplication.java
@@ -0,0 +1,13 @@
+package com.usinsa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsinsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsinsaApplication.class, args);
+    }
+
+}
