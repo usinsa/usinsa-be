57ecf396f96836c927c74bf7efe3aa648961ced6
kozae00 <rhwogus777@gmail.com>
2025-09-23 23:11:58 +0900

    feat: repo/service create

--- src/main/java/com/usinsa/backend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/usinsa/backend/domain/member/repository/MemberRepository.java
@@ -4,5 +4,7 @@ import com.usinsa.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByUsinaId(String usinaId);
 
 }
--- src/main/java/com/usinsa/backend/domain/member/service/MemberService.java
+++ src/main/java/com/usinsa/backend/domain/member/service/MemberService.java
@@ -1,4 +1,39 @@
 package com.usinsa.backend.domain.member.service;
 
+import com.usinsa.backend.domain.member.entity.Member;
+import com.usinsa.backend.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Member create(Member member) {
+
+        if (memberRepository.existsByEmail(member.getEmail())) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        }
+        if (memberRepository.existsByUsinaId(member.getUsinaId())) {
+            throw new IllegalArgumentException("이미 사용 중인 유신아이디입니다.");
+        }
+
+        Member toSave = Member.builder()
+                .usinaId(member.getUsinaId())
+                .password(member.getPassword())
+                .name(member.getName())
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .phone(member.getPhone())
+                .profileImage(member.getProfileImage())
+                .isAdmin(false)
+                .build();
+
+        return memberRepository.save(toSave);
+    }
+
 }
