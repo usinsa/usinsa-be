9aedf9ecc426d33680714a0729e52381393c6ebf
linedj <eunjun998@gmail.com>
2025-09-23 22:30:34 +0900

    feat: create Product Controller&Service without DTO

---
+++ src/main/java/com/usinsa/backend/domain/product/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.usinsa.backend.domain.product.controller;
+
+import com.usinsa.backend.domain.product.entity.Product;
+import com.usinsa.backend.domain.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    /* ********************DTO 추가시 변경***************** */
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(
+            @RequestParam Long categoryId,
+            @RequestParam String name,
+            @RequestParam String brand,
+            @RequestParam Integer price) {
+
+        Product product = productService.createProduct(categoryId, name, brand, price);
+        return ResponseEntity.ok(product);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+}
