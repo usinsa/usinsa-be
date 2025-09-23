2f29c67ab94f10ae12cbbef4e21151ad31526314
kozae00 <rhwogus777@gmail.com>
2025-09-23 17:22:29 +0900

    feat: MemberRepository create

--- src/main/java/com/usinsa/backend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/usinsa/backend/domain/member/repository/MemberRepository.java
@@ -1,4 +1,8 @@
 package com.usinsa.backend.domain.member.repository;
 
-public class MemberRepository {
+import com.usinsa.backend.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }
