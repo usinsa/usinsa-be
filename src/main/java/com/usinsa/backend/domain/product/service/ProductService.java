3e4d07c69c05031307df95ecccdc308c5fe89f99
linedj <108262570+linedj@users.noreply.github.com>
2025-09-24 13:55:17 +0900

    Feat/#2,3 product&category 도메인 생성  (#6)
    * feat: create Product&Category Entity
    
    * feat: create  Product&Category Repository
    
    * feat: create Product Controller&Service without DTO
    
    * fix: pric typr Integer -> Long

---
+++ src/main/java/com/usinsa/backend/domain/product/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.usinsa.backend.domain.product.service;
+
+import com.usinsa.backend.domain.category.entity.Category;
+import com.usinsa.backend.domain.category.repository.CategoryRepository;
+import com.usinsa.backend.domain.product.entity.Product;
+import com.usinsa.backend.domain.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    /* ********************DTO 추가시 변경***************** */
+
+    // 상품 등록
+    @Transactional
+    public Product createProduct(Long categoryId, String name, String brand, Long price) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new IllegalArgumentException("카테고리를 찾을 수 없습니다."));
+
+        Product product = Product.builder()
+                .name(name)
+                .brandName(brand)
+                .price(price)
+                .category(category)
+                .likeCount(0)
+                .clickCount(0)
+                .build();
+
+        return productRepository.save(product);
+    }
+
+    // 상품 조회
+    @Transactional(readOnly = true)
+    public Product getProductById(Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
+    }
+}
