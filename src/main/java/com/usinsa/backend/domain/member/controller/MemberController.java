e98c7dedfe09250d0f1275b39ccb7789f741c23e
kozae00 <rhwogus777@gmail.com>
2025-09-26 01:12:15 +0900

    feat: singup controller

--- src/main/java/com/usinsa/backend/domain/member/controller/MemberController.java
+++ src/main/java/com/usinsa/backend/domain/member/controller/MemberController.java
@@ -1,6 +1,9 @@
 package com.usinsa.backend.domain.member.controller;
 
+import com.usinsa.backend.domain.member.dto.MemberResDto;
+import com.usinsa.backend.domain.member.dto.SingupReqDto;
 import com.usinsa.backend.domain.member.entity.Member;
+import com.usinsa.backend.domain.member.repository.MemberRepository;
 import com.usinsa.backend.domain.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +19,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResDto> signup(@Valid @RequestBody SingupReqDto singupReqDto) {
+        MemberResDto created = memberService.singup(singupReqDto);
+        return ResponseEntity.ok(created);
+    }
 
     @PostMapping("/login")
     public ResponseEntity<Member> login(@Valid @RequestBody Member member) {
