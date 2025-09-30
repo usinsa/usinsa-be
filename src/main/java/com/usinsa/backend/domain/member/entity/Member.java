978bf0a658cbfbff92b0b9dab80bd433d8b551c0
kozae00 <rhwogus777@gmail.com>
2025-10-01 02:40:44 +0900

    refactor: imoprt adding

--- src/main/java/com/usinsa/backend/domain/member/entity/Member.java
+++ src/main/java/com/usinsa/backend/domain/member/entity/Member.java
@@ -1,6 +1,7 @@
 package com.usinsa.backend.domain.member.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
