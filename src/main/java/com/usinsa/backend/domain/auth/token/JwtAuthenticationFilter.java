0db212367cf4664ca46289885d11b7a2477a7cd1
kozae00 <rhwogus777@gmail.com>
2025-10-01 03:10:28 +0900

    feat: filter

--- src/main/java/com/usinsa/backend/domain/auth/token/JwtAuthenticationFilter.java
+++ src/main/java/com/usinsa/backend/domain/auth/token/JwtAuthenticationFilter.java
@@ -1,4 +1,50 @@
 package com.usinsa.backend.domain.auth.token;
 
-public class JwtAuthenticationFilter {
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwt;
+    private final CustomUserDetailsService uds;
+    private final TokenStore tokenStore;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws ServletException, IOException {
+        String header = req.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            try {
+                jwt.parseAccess(token); // 서명/만료 검증
+                if (tokenStore.isBlacklisted(token)) throw new SecurityException("Token blacklisted");
+                String email = jwt.parseAccess(token).getBody().getSubject();
+
+                UserDetails user = uds.loadUserByUsername(email);
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (JwtException | SecurityException ignored) {
+
+            }
+        }
+        chain.doFilter(req, res);
+    }
 }
+
