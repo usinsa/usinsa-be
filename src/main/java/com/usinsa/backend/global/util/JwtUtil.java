3eb9cb35d0b9bb04ccadeb58639dc2f60ca6ea25
kozae00 <rhwogus777@gmail.com>
2025-10-02 20:13:04 +0900

    feat: JwtUtil

--- src/main/java/com/usinsa/backend/global/util/JwtUtil.java
+++ src/main/java/com/usinsa/backend/global/util/JwtUtil.java
@@ -1,4 +1,22 @@
 package com.usinsa.backend.global.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
 public class JwtUtil {
+    public static String createToken(Key secretKey, int expireSeconds, Map<String, Object> claims) {
+        Date issuedAt = new Date();
+        Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
