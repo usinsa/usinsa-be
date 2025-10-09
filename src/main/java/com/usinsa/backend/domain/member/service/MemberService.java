31f49178b9b8c00d7abb63e7c6010d97f472c6d5
kozae00 <rhwogus777@gmail.com>
2025-10-09 18:02:03 +0900

    refactor: raw.equals(encoded) -> PasswordEncoder.matches()

--- src/main/java/com/usinsa/backend/domain/member/service/MemberService.java
+++ src/main/java/com/usinsa/backend/domain/member/service/MemberService.java
@@ -6,6 +6,7 @@ import com.usinsa.backend.domain.member.dto.SignupReqDto;
 import com.usinsa.backend.domain.member.entity.Member;
 import com.usinsa.backend.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -42,14 +43,18 @@ public class MemberService {
         return MemberResDto.from(toSave);
     }
 
-
-    @Transactional
+    /*
+     * 비밀번호를 비교할 때, DB에는 평문이 아닌 해시로 저장됨
+     * 그렇기에 PasswordEncoder.matches(raw, encoded)를 사용함.
+     * equals를 사용해 비밀번호 일치 여부를 확인하면 평문이랑 해시를 비교하는 상황이 발생함.
+     */
+    @Transactional(readOnly = true)
     public Member login(LoginReqDto loginReqDto) {
         Member member = memberRepository.findByUsinaId(loginReqDto.getUsinaId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유신아이디입니다."));
 
-        if (!member.getPassword().equals(loginReqDto.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        if (!passwordEncoder.matches(loginReqDto.getPassword(), member.getPassword())) {
+            throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
         }
 
         return member;
