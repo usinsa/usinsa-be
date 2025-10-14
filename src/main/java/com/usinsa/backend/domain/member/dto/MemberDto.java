f13dcafc20be4abd96f076fba5b0b8cf1c96582a
kozae00 <rhwogus777@gmail.com>
2025-10-15 01:35:00 +0900

    feat: MemberDto

---
+++ src/main/java/com/usinsa/backend/domain/member/dto/MemberDto.java
@@ -0,0 +1,51 @@
+package com.usinsa.backend.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class MemberDto {
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class CreateReq{
+        private String usinaId;
+        private String password;
+        private String name;
+        private String nickname;
+        private String email;
+        private String phone;
+        private String profileImage;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class UpdateReq{
+        private String password;
+        private String name;
+        private String nickname;
+        private String email;
+        private String phone;
+        private String profileImage;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Resopnse {
+        private Long Id;
+        private String usinaId;
+        private String name;
+        private String nickname;
+        private String email;
+        private String phone;
+        private String profileImage;
+        private Boolean isAdmin;
+    }
+}
