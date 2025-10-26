adbefa55b58ac558bdf517a1967f3b3a39343404
kozae00 <rhwogus777@gmail.com>
2025-10-26 17:00:51 +0900

    feat: component + redis

--- src/main/java/com/usinsa/backend/global/security/token/store/TokenBlacklist.java
+++ src/main/java/com/usinsa/backend/global/security/token/store/TokenBlacklist.java
@@ -1,10 +1,7 @@
 package com.usinsa.backend.global.security.token.store;
 
-import org.springframework.stereotype.Repository;
-
 import java.time.Instant;
 
-@Repository
 public interface TokenBlacklist {
     void blacklist(String jti, Instant expiresAt);  // access jti -> 만료시각
     boolean isBlacklisted(String jti);
