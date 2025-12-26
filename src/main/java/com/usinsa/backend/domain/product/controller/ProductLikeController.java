443910221458150232719f4a730ce8884af7b6b8
linedj <eunjun998@gmail.com>
2025-12-26 22:51:52 +0900

    fix: productlike API mapping

--- src/main/java/com/usinsa/backend/domain/product/controller/ProductLikeController.java
+++ src/main/java/com/usinsa/backend/domain/product/controller/ProductLikeController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductLikeController {
 
