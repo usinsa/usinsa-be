ba484664e01d67db86704f4003b44fcece1e8259
linedj <eunjun998@gmail.com>
2026-04-14 18:25:48 +0900

    feat: CORS REINDEXING

--- src/main/java/com/usinsa/backend/global/config/SecurityConfig.java
+++ src/main/java/com/usinsa/backend/global/config/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig {
                     "/api/v1/auth/logout",
                     "/api/v1/auth/signup",
                     "/api/v1/auth/refresh",
-                    "/api/v1/auth/me"
+                    "/api/v1/auth/me",
+                    "/api/v1/products/reindex"
                 ).permitAll()
                 // OAuth2
                 .requestMatchers(
